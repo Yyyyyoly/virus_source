957f428b2a386ca3f0e651350e8f35c3ef54cb0d
PythonCakeMing <1851464029@qq.com>
2017-09-25 15:13:47 +0800

    customer service

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -6,6 +6,10 @@ exports.instruction = function (req, res) {
   res.render('instruction', {title: 'instruction page'});
 };
 
+exports.customerService = function (req, res) {
+  res.render('customerService', {title: 'instruction page'});
+};
+
 exports.about = function (req, res) {
   res.render('about', {title: 'about page'});
 };
--- app/routes/index.route.js
+++ app/routes/index.route.js
@@ -4,6 +4,7 @@ const indexController = require('../controllers/index.controller');
 const prefix = '/';
 router.get('/', indexController.index);
 router.get('/instruction', indexController.instruction);
+router.get('/customer-service', indexController.customerService);
 router.get('/about', indexController.about);
 
 module.exports = function (app) {
