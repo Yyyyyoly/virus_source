295a09480beb80438ba2a3d0e968ab91e52c6312
PythonCakeMing <1851464029@qq.com>
2017-09-25 11:06:36 +0800

    server init

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+const express = require('./config/express');
+const mongoose = require('./config/mongoose');
+const config = require('./config/config');
 
-console.log(`Server running at http://localhost:`);
+// mongodb must go first
+const monogdb = mongoose();
+const app = express();
+app.listen(config.serverPort);
+
+module.exports = app;
+console.log(`Server running at http://localhost:${config.serverPort}`);
