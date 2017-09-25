5ae259c6960569f96efc9cd6952d655b907bf3b2
PythonCakeMing <1851464029@qq.com>
2017-09-25 14:12:37 +0800

    mysql fix

--- config/mysql.js
+++ config/mysql.js
@@ -1,17 +1,15 @@
 const config = require('./config');
 const Sequelize = require('sequelize');
 
-module.exports = function () {
-  const mysql = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    {
-      'dialect': 'mysql',
-      'host': config.host,
-      'port': config.port,
-    }
-  );
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    'dialect': 'mysql',
+    'host': config.host,
+    'port': config.port,
+  }
+);
 
-  return mysql;
-};
+module.exports = sequelize;
