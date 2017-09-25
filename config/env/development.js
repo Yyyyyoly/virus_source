c1acfee89bafb3617345abb1b16c81b52c54cd43
PythonCakeMing <1851464029@qq.com>
2017-09-25 09:51:27 +0800

    development config

---
+++ config/env/development.js
@@ -0,0 +1,31 @@
+module.exports = {
+  // development configuration options
+  serverPort: 3000,
+  domain: 'http://localhost:3000',
+  sessionSecret: 'xsm_development_secret',
+
+  // db config
+  mongoUrl: 'mongodb://localhost/virus_source_app',
+
+  // redis config
+  redisConfig: {
+    host: '127.0.0.1',
+    port: '6379',
+    pass: '',
+    ttl: 1800,
+    logErrors: true,
+  },
+
+  // ali dayu message config
+  messageConfig: {
+    accessKeyId: 'LTAIFC2edkuCup8n',
+    secretAccessKey: 'P1fm0jjHJqDPlL892Dcjr0rGnqo2xb',
+    MessageTemplete: 'SMS_94765019',
+    MessageSignName: '迈德科技',
+  },
+
+  // baidu Map config
+  mapConfig: {
+    ak: 'NH5B0cjmPzbtTuyl1xswwlOIPWvHisdA',
+  },
+};
