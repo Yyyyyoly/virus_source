0fe6ecd9d6fdf8f2ad746c359d60ec692de9e954
PythonCakeMing <1851464029@qq.com>
2017-09-25 17:13:49 +0800

    fix error

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const config = require('./config/config');
-const http = require('http');
 const models = require('./app/models');
-const express = require('./config/express');
-
-const app = express();
-const server = http.createServer(app);
+const app = require('./config/express')();
 
 models.sequelize.sync().then(function () {
-  server.listen(config.serverPort, function () {
+  app.listen(config.serverPort, function () {
     console.log(`Server running at ${config.serverHost}:${config.serverPort}`);
   });
 });
