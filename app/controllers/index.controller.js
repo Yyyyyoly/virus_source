07a0cb6383ddee69236b276111636804cd66b3ed
PythonCakeMing <1851464029@qq.com>
2017-09-25 15:09:33 +0800

    instruction

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -2,6 +2,10 @@ exports.index = function (req, res) {
   res.render('index', {title: 'index page'});
 };
 
+exports.instruction = function (req, res) {
+  res.render('instruction', {title: 'instruction page'});
+};
+
 exports.about = function (req, res) {
   res.render('about', {title: 'about page'});
 };
