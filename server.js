dda1412c3bfadc9b1eeee11d5585909bc0c5915c
wangyg <wyg569983202@qq.com>
2017-11-24 17:28:34 +0800

    nginx开启了ssl

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ process.env.TZ = 'Asia/Shanghai';
 const config = require('./config/config');
 const models = require('./app/models');
 const app = require('./config/express')();
-// 项目内加openssl
-const https = require('https');
-const fs = require('fs');
-
-const credentials = {
-  key: fs.readFileSync('./pem/214354661570662.key'),
-  cert: fs.readFileSync('./pem/214354661570662.pem'),
-};
-const httpsServer = https.createServer(credentials, app);
+// // 项目内加openssl
+// const https = require('https');
+// const fs = require('fs');
+//
+// const credentials = {
+//   key: fs.readFileSync(`${__dirname}/pem/214354661570662.key`),
+//   cert: fs.readFileSync(`${__dirname}/pem/214354661570662.pem`),
+// };
+// const httpsServer = https.createServer(credentials, app);
 
 models.sequelize.sync().then(() => {
-  httpsServer.listen(config.serverPort, () => {
+  app.listen(config.serverPort, () => {
     console.log(`Server running at ${config.serverHost}:${config.serverPort}`);
   });
 });
