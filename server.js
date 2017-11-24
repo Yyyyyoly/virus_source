068976d8ad1c60862ae2d1b32a79348993f274a8
wangyg <wyg569983202@qq.com>
2017-11-24 14:10:52 +0800

    fix err

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const config = require('./config/config');
 const models = require('./app/models');
 const app = require('./config/express')();
 // 项目内加openssl
-// const https = require('https');
-// const fs = require('fs');
-//
-// const credentials = {
-//   key: fs.readFileSync('./pem/privatekey.pem'),
-//   cert: fs.readFileSync('./pem/certificate.pem'),
-// };
-// const httpsServer = https.createServer(credentials, app);
+const https = require('https');
+const fs = require('fs');
+
+const credentials = {
+  key: fs.readFileSync('./pem/privatekey.pem'),
+  cert: fs.readFileSync('./pem/certificate.pem'),
+};
+const httpsServer = https.createServer(credentials, app);
 
 models.sequelize.sync().then(() => {
-  app.listen(config.serverPort, () => {
+  httpsServer.listen(config.serverPort, () => {
     console.log(`Server running at ${config.serverHost}:${config.serverPort}`);
   });
 });
