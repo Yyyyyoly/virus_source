170dcfdd0ed27fb7c03618b58529a1f48f8f0915
wangyg <wyg569983202@qq.com>
2017-12-13 10:21:38 +0800

    因为迈德医学项目数据都在redis 1库 所以我为了区分开把迈德分享的业务数据存到了4库

--- config/redis.js
+++ config/redis.js
@@ -6,17 +6,17 @@ global.globalRedisClient = {};
 
 // 目前:
 // redis-session存在0库  这个主要存储session
-// 业务数据记录在1库
+// 业务数据记录在4库(之前在1库，后来发现迈德医学项目用的全是1库，就换了个位置)
 // 获取微信信息的token存在2库
-
 // 所有项目的总积分存在3库里 便于通用积分商城的使用
+
 module.exports = function (type) {
   const redisConfig = config.redisConfig || { host: '127.0.0.1', port: '6379' };
   const clientType = type ? parseInt(type, 0) : 0;
 
   if (!global.globalRedisClient[clientType]) {
     if (clientType === 1) {
-      redisConfig.db = 1;
+      redisConfig.db = 4;
     } else if (clientType === 2) {
       redisConfig.db = 2;
     } else if (clientType === 3) {
