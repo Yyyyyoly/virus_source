795e759b4cc10047c586602762e440932b5ba022
wangyg <wyg569983202@qq.com>
2017-12-20 12:22:10 +0800

    优化代码

--- config/redis.js
+++ config/redis.js
@@ -2,19 +2,21 @@ const redis = require('redis');
 const bluebird = require('bluebird');
 const config = require('./config');
 
-global.globalRedisClient = {};
-
+// note: nodejs和php不同之处：
+// 如果是php，这里一定要是全局变量才能实现单例模式，
+// 而nodejs exports以后每次读的都是上次的缓存
+const globalRedisClient = {};
 // 目前:
 // redis-session存在0库  这个主要存储session
 // 业务数据记录在4库
 // 获取微信信息的token存在2库
 
 // 所有项目的总积分存在3库里 便于通用积分商城的使用
-module.exports = function (type) {
+module.exports = function (type = 0) {
   const redisConfig = config.redisConfig || { host: '127.0.0.1', port: '6379' };
   const clientType = type ? parseInt(type, 0) : 0;
 
-  if (!global.globalRedisClient[clientType]) {
+  if (!globalRedisClient[clientType]) {
     if (clientType === 1) {
       redisConfig.db = 4;
     } else if (clientType === 2) {
@@ -33,8 +35,8 @@ module.exports = function (type) {
     client.on('error', (err) => {
       console.log(`Error: ${err}`);
     });
-    global.globalRedisClient[clientType] = client;
+    globalRedisClient[clientType] = client;
   }
 
-  return global.globalRedisClient[clientType];
+  return globalRedisClient[clientType];
 };
